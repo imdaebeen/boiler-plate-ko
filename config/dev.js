5763910b02327225fecc75ef811c7496b881a83b
imdaebeenm <imdaebeen_09@naver.com>
2021-02-24 23:40:00 +0900

    비밀 정보 보호

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : 'mongodb+srv://imdaebeen_09:snvl2455!@cluster0.9ddix.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -2,8 +2,18 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const config = require('./config/key');
+const bodyParser = require('body-parser');
+const{User} = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+//application/json
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://imdaebeen_09:snvl2455!@cluster0.9ddix.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
   useNewUrlParser: true,useUnifiedTopology:true,useCreateIndex:true,useFindAndModify:false
 }).then(()=> console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
@@ -12,6 +22,20 @@ mongoose.connect('mongodb+srv://imdaebeen_09:snvl2455!@cluster0.9ddix.mongodb.ne
 
 app.get('/', (req, res) => {
   res.send('Hello World!~~ 안녕하세요 ~')
+})
+
+app.post('/register',(req,res) =>{
+    
+  //회원 가입 할때 필요한 정보들을 client에서 가져오면 
+  //그것들을 데이터 베이스에 넣어준다.
+  const user = new User(req.body)
+  
+  user.save((err,userInfo) =>{
+      if(err) return res.json({success:false,err})
+      return res.status(200).json({
+        success:true
+      })
+  })
 })
 
 app.listen(port, () => {
