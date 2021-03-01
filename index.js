bd17feb423ff4276d2449ccd2a470de73d0dae7e
imdaebeenm <imdaebeen_09@naver.com>
2021-03-01 21:57:12 +0900

    로그아웃 기능 권한 기능 추가

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ const port = 5000
 const config = require('./config/key');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const{User} = require("./models/User");
+const { auth } = require('./middleware/auth')
+const{ User } = require("./models/User");
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended:true}));
 
 //application/json
 app.use(bodyParser.json());
-app.use(cookieParser);
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
@@ -26,7 +27,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!~~ 안녕하세요 ~')
 })
 
-app.post('/register',(req,res) =>{
+app.post('/api/users/register',(req,res) =>{
     
   //회원 가입 할때 필요한 정보들을 client에서 가져오면 
   //그것들을 데이터 베이스에 넣어준다.
@@ -40,7 +41,7 @@ app.post('/register',(req,res) =>{
   })
 })
 
-app.post('/login',(req,res) => {
+app.post('/api/users/login',(req,res) => {
   
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
   User.findOne({ email: req.body.email }, (err,user) => {
@@ -68,6 +69,35 @@ app.post('/login',(req,res) => {
    })
  })
 })
+
+// role 1 어드민 role 2 특정 부서 어드민
+// role 0 -> 일반유저 role 0이 아니면 관리자
+
+app.get('/api/users/auth',auth,(req,res) => {
+
+  //여기 까지 미틀웨어를 통과해 왔다는 애기는 Authentication이 True 라는 말.
+  res.status(200).json({
+    _id:req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email:req.user.email,
+    name : req.user.name,
+    lastname : req.user.lastname,
+    image: req.user.image
+  })
+})
+
+app.get('/api/users/logout',auth,(req,res) => {
+  User.findOneAndUpdate({_id: req.user._id},
+    {token: ""}
+    ,(err,user) => {
+      if(err) return res.json({success:false,err});
+      return res.status(200).send({
+        success:true
+      })
+    })
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
